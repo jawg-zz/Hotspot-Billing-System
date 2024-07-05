55276d8495ffae559e26b7d287f2a5b2e77ebf10
MICHAEL JAMES <97917001+MJCappella@users.noreply.github.com>
2024-07-05 17:04:41 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,6 @@
  *  PHP Mikrotik Billing (https://github.com/hotspotbilling/phpnuxbill/)
  *  by https://t.me/ibnux
  **/
-
+//add this to mikrotik router to redirect users
 
 header('location: ../index.php?_route=admin/');
