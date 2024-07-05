c6a546c82c9abf01361b44d88d92281094eb9599
MICHAEL JAMES <97917001+MJCappella@users.noreply.github.com>
2024-07-05 17:08:29 +0300

    Update step2.php

--- install/step2.php
+++ install/step2.php
@@ -61,7 +61,7 @@
             ?>
         </div>
     </div>
-    <div class="footer">Copyright &copy; 2021 PHPNuxBill. All Rights Reserved<br /><br /></div>
+    <div class="footer">Copyright &copy; 2024 PHPNuxBill. All Rights Reserved<br /><br /></div>
 </body>
 
 </html>
